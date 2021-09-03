ffe8693eb394cce5ce8c9f31213cc861ee797a46
Rito (Baidao) Cha <90030036+RitoNQ@users.noreply.github.com>
2021-09-03 08:06:01 +0000

    Create alitt.go

---
+++ alitt.go
@@ -0,0 +1,10 @@
+// RegexMatch reports whether the string s contains any match of the regular expression pattern.
+func RegexMatch(pat, s string) (bool, error) {
+	val, err, _ := regexGroup.Do(pat, func() (interface{}, error) {
+		return regexp.Compile(pat)
+	})
+	if err != nil {
+		return false, err
+	}
+	return val.(*regexp.Regexp).MatchString(s), nil
+}
