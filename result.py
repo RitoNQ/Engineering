b14be970aa55426c51c31b4141d3f2732d5d8ee1
Rito (Baidao) Cha <90030036+RitoNQ@users.noreply.github.com>
2021-09-03 08:06:37 +0000

    Create result.py

---
+++ result.py
@@ -0,0 +1,6 @@
+a = '''
+BenchmarkRegexMatch-16    	 1299758	      1854 ns/op	    1551 B/op	      18 allocs/op
+BenchmarkRegexMatch-16    	 1218178	      1998 ns/op	    1649 B/op	      19 allocs/op
+'''
+
+print(a)
